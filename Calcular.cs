4c8865d63d5df0e66409bc4c3be86a0f3cdefa53
joseivandejesusenriquez <91434749+joseivandejesusenriquez@users.noreply.github.com>
2021-09-26 15:12:16 -0500

    Add files via upload

---
+++ Calcular.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UNACH.CALCULADORA.WS.Clases
+{
+    public class Calcular
+    {
+        public decimal lado1 { get; set; }
+        public decimal lado2 { get; set; }
+        public double radio1 { get; set; }
+        public decimal AreaCuadrado()
+        {
+            return lado1 * lado2;
+        }
+
+        public decimal triangulo(decimal basee, decimal altura)
+        {
+            lado1 = basee;
+            lado2 = altura;
+            return basee * altura / 2;
+        }
+      
+  
+            public double circulo(double radio)
+            {
+                double pi = 3.1416;
+                radio1 = radio;
+                return (radio * radio) * pi;
+
+            }
+        
+        
+    }
+
+}
---
+++ CalcularService.asmx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using UNACH.CALCULADORA.WS.Clases;
+
+namespace UNACH.CALCULADORA.WS
+{
+    /// <summary>
+    /// Descripción breve de CalcularService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalcularService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+        [WebMethod(Description = "Este metodo calcula el area de un cuadrado")]
+        public decimal cuadrado(decimal valor1, decimal valor2)
+        {
+            Calcular calcula = new Calcular();
+            calcula.lado1 = valor1;
+            calcula.lado2 = valor2;
+            return calcula.AreaCuadrado();
+        }
+        [WebMethod(Description = "Este metodo calcula el area de un triangulo")]
+        public decimal trianguloo(decimal baseee, decimal alturaa)
+        {
+            Calcular calcula = new Calcular();
+            return calcula.triangulo(baseee, alturaa);
+        }
+        [WebMethod(Description = "Este metodo calcula el area de un circulo")]
+        public double AreaCirculo(double radioo)
+        {
+            Calcular calcula = new Calcular();
+            return calcula.circulo(radioo);
+        }
+    }
+}
